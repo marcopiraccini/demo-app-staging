d723acca68fbc2e70e9ebd4890f9c762cd89edad
marcopiraccini <marco.piraccini@gmail.com>
2023-05-24 10:45:58 +0200

    deploy test

--- plugin.js
+++ plugin.js
@@ -3,5 +3,5 @@
 
 /** @param {import('fastify').FastifyInstance} app */
 module.exports = async function (app) {
-  console.log('The plugin has been loaded 17')
+  console.log('The plugin has been loaded 18')
 }
